18f1687503009500a3683f9622e319fadec2fbd3
Shadizzle <mail@shayden.me>
2017-11-25 17:35:38 +0930

    First steps setting up canvas done

--- src/webgl_cljs/core.cljs
+++ src/webgl_cljs/core.cljs
@@ -1,5 +1,30 @@
 (ns webgl-cljs.core)
 
+(defn- fClear []
+  (this-as this
+    (.clear this
+      (bit-or (.-COLOR_BUFFER_BIT this)
+              (.-DEPTH_BUFFER_BIT this)))
+    this))
+
+(defn- fSetSize [w, h]
+  (this-as this
+    (set! (.. this -canvas -style -width) (str w "px"))
+    (set! (.. this -canvas -style -height) (str h "px"))
+    (set! (.. this -canvas -width) w)
+    (set! (.. this -canvas -height) h)
+    (.viewport this 0 0 w h)
+    this))
+
+(defn init-gl-context [canvas]
+  (doto (.getContext canvas "webgl2")
+    (.clearColor 1.0 1.0 1.0 1.0)
+    (aset "fClear" fClear)
+    (aset "fSetSize" fSetSize)))
+
 (defn -main [& args]
-  (let [root (js/document.getElementById "root")]
-    (aset root "innerHTML" "<p>I'm dynamically created.</p>")))
+  (let [root (js/document.getElementById "root")
+        gl (init-gl-context root)]
+    (doto gl
+      (.fSetSize 500 500)
+      (.clear))))
