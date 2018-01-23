a52343b2f2b4bb8aebb00ad93edeb2d2b45fab6e
Shadizzle <mail@shayden.me>
2018-01-23 23:57:23 +0930

    Copied into cljs and annotated drawing a triangle, however it does not appear to render correctly

--- src/webgl_cljs/core.cljs
+++ src/webgl_cljs/core.cljs
@@ -1,30 +1,75 @@
 (ns webgl-cljs.core)
 
-(defn- fClear []
-  (this-as this
-    (.clear this
-      (bit-or (.-COLOR_BUFFER_BIT this)
-              (.-DEPTH_BUFFER_BIT this)))
-    this))
+(def vertices [-0.5  0.5  0.0
+               -0.5 -0.5  0.0
+                0.5 -0.5  0.0])
 
-(defn- fSetSize [w, h]
-  (this-as this
-    (set! (.. this -canvas -style -width) (str w "px"))
-    (set! (.. this -canvas -style -height) (str h "px"))
-    (set! (.. this -canvas -width) w)
-    (set! (.. this -canvas -height) h)
-    (.viewport this 0 0 w h)
-    this))
+(def indices [0, 1, 2])
 
-(defn init-gl-context [canvas]
-  (doto (.getContext canvas "webgl2")
-    (.clearColor 1.0 1.0 1.0 1.0)
-    (aset "fClear" fClear)
-    (aset "fSetSize" fSetSize)))
+(def vert-code
+  (str
+   "attribute vec3 coordinates;"
+   "void main(void) {"
+   " gl_Position = vec4(coordinates, 1.0);"
+   "}"))
+
+(def frag-code
+  (str
+   "void main(void) {"
+   " gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);"
+   "}"))
 
 (defn -main [& args]
   (let [root (js/document.getElementById "root")
-        gl (init-gl-context root)]
+        gl   (.getContext root "webgl")
+        vertex-buffer (.createBuffer gl)
+        index-buffer  (.createBuffer gl)
+        vert-shader   (.createShader gl (.-VERTEX_SHADER gl))
+        frag-shader   (.createShader gl (.-FRAGMENT_SHADER gl))
+        shader-program (.createProgram gl)]
     (doto gl
-      (.fSetSize 500 500)
-      (.clear))))
+      ;; Bind, write vertices to ARRAY_BUFFER, unbind
+      (.bindBuffer (.-ARRAY_BUFFER gl) vertex-buffer)
+      (.bufferData (.-ARRAY_BUFFER gl)
+                   (js/Float32Array. vertices)
+                   (.-STATIC_DRAW gl))
+      (.bindBuffer (.-ARRAY_BUFFER gl) nil)
+      ;; Bind, write indices to ELEMENT_ARRAY_BUFFER, unbind
+      (.bindBuffer (.-ELEMENT_ARRAY_BUFFER gl) index-buffer)
+      (.bufferData (.-ELEMENT_ARRAY_BUFFER gl)
+                   (js/Uint16Array. indices)
+                   (.-STATIC_DRAW gl))
+      (.bindBuffer (.-ELEMENT_ARRAY_BUFFER gl) nil)
+      ;; Assign vertex code to shader and compile
+      (.shaderSource vert-shader vert-code)
+      (.compileShader vert-shader)
+      ;; Assign fragment code to shader and compile
+      (.shaderSource frag-shader frag-code)
+      (.compileShader frag-shader)
+      ;; Attach both shaders, link and use program
+      (.attachShader shader-program vert-shader)
+      (.attachShader shader-program frag-shader)
+      (.linkProgram shader-program)
+      (.useProgram shader-program)
+      ;; Bind both buffers
+      (.bindBuffer (.-ARRAY_BUFFER gl) vertex-buffer)
+      (.bindBuffer (.-ELEMENT_ARRAY_BUFFER gl) index-buffer))
+    ;; ???
+    (let [coord (.getAttribLocation gl shader-program "coordinates")]
+      (doto gl
+        (.vertexAttribPointer coord 3 (.-FLOAT gl) false 0 0)
+        (.enableVertexAttribArray coord)))
+    (doto gl
+      ;; Clear canvas
+      (.clearColor 0.5 0.5 0.5 0.9)
+      ;; Enable depth test?
+      (.enable (.-DEPTH_TEST gl))
+      ;; Clear color buffer bit?
+      (.clear (.-COLOR_BUFFER_BIT gl))
+      ;; Set viewport
+      (.viewport 0 0 (.-width root) (.-height root))
+      ;; Draw triangle
+      (.drawElements (.-TRIANGLES gl)
+                     (.-length indices)
+                     (.-UNSIGNED_SHORT gl)
+                     0))))
